9a74cb0e338f1409a68276e98e1113672cf64086
Tomasz Jaworski <tj.toja@gmail.com>
2021-12-05 12:20:04 +0100

    Added if statemnt for X Wins in Game.java

--- src/main/java/com/udacity/Game.java
+++ src/main/java/com/udacity/Game.java
@@ -154,6 +154,16 @@ public class Game {
     public String checkGameWinner(char[][] grid) {
         String result = "None";
         //Student code goes here ...
+        if ((grid[0][0] == 'x' && grid[1][1] == 'x' && grid[2][2] == 'x') ||
+                (grid[2][0] == 'x' && grid[1][1] == 'x' && grid[0][2] == 'x') ||
+                (grid[0][0] == 'x' && grid[0][1] == 'x' && grid[0][2] == 'x') ||
+                (grid[1][0] == 'x' && grid[1][1] == 'x' && grid[1][2] == 'x') ||
+                (grid[2][0] == 'x' && grid[2][1] == 'x' && grid[2][2] == 'x') ||
+                (grid[0][0] == 'x' && grid[1][0] == 'x' && grid[2][0] == 'x') ||
+                (grid[0][1] == 'x' && grid[1][1] == 'x' && grid[2][1] == 'x')||
+                (grid[0][2] == 'x' && grid[1][2] == 'x' && grid[2][2] == 'x')) {
+            result = "X wins";
+        }
 
         return result;
     }
