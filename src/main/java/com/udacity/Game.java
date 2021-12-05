c44262e07c48ab9be4c15b4a98562e882d7099e9
Tomasz Jaworski <tj.toja@gmail.com>
2021-12-05 17:33:45 +0100

    Added if statemnt for X Wins in Game.java

--- src/main/java/com/udacity/Game.java
+++ src/main/java/com/udacity/Game.java
@@ -160,11 +160,19 @@ public class Game {
                 (grid[1][0] == 'x' && grid[1][1] == 'x' && grid[1][2] == 'x') ||
                 (grid[2][0] == 'x' && grid[2][1] == 'x' && grid[2][2] == 'x') ||
                 (grid[0][0] == 'x' && grid[1][0] == 'x' && grid[2][0] == 'x') ||
-                (grid[0][1] == 'x' && grid[1][1] == 'x' && grid[2][1] == 'x')||
+                (grid[0][1] == 'x' && grid[1][1] == 'x' && grid[2][1] == 'x') ||
                 (grid[0][2] == 'x' && grid[1][2] == 'x' && grid[2][2] == 'x')) {
             result = "X wins";
+        } else if ((grid[0][0] == 'o' && grid[1][1] == 'o' && grid[2][2] == 'o') ||
+                (grid[2][0] == 'o' && grid[1][1] == 'o' && grid[0][2] == 'o') ||
+                (grid[0][0] == 'o' && grid[0][1] == 'o' && grid[0][2] == 'o') ||
+                (grid[1][0] == 'o' && grid[1][1] == 'o' && grid[1][2] == 'o') ||
+                (grid[2][0] == 'o' && grid[2][1] == 'o' && grid[2][2] == 'o') ||
+                (grid[0][0] == 'o' && grid[1][0] == 'o' && grid[2][0] == 'o') ||
+                (grid[0][1] == 'o' && grid[1][1] == 'o' && grid[2][1] == 'o') ||
+                (grid[0][2] == 'o' && grid[1][2] == 'o' && grid[2][2] == 'o')) {
+            result = "O wins";
         }
-
         return result;
     }
 
